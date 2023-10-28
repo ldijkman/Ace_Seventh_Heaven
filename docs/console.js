d24fbcf8671b6aac39fcd7186850422eb4d49917
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2023-10-28 09:35:47 +0200

    Update console.js

--- docs/console.js
+++ docs/console.js
@@ -19,7 +19,10 @@ creates a console like next
   </div>
 </div>
 */
-
+    document.addEventListener("DOMContentLoaded",
+      function (event) {
+            dragElement(document.getElementById("console-log-div")); 
+    });
 
 function hideconsole() {
   document.getElementById("console-log-div").style.display = "none";
@@ -27,7 +30,66 @@ function hideconsole() {
 function showconsole() {
   document.getElementById("console-log-div").style.display = "block";
 }
-                                                   
+
+
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+//  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+  //  document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+ // } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    document.getElementById("legend").onmousedown = dragMouseDown;
+ 
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // jQuery load complete, do your magic                  //https://stackoverflow.com/questions/7496789/how-to-include-jquery-in-another-javascript-file
 
 
