3a75a1f5518608321e560d3b25918684f54c1e3a
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2023-10-28 19:46:05 +0200

    Update console.js

--- docs/console.js
+++ docs/console.js
@@ -21,7 +21,7 @@ creates a console like next
 */
     document.addEventListener("DOMContentLoaded",
       function (event) {
-            dragElement(document.getElementById("console-log-div")); 
+            dragElement(document.getElementById("console-log-div")); //https://www.w3schools.com/howto/howto_js_draggable.asp
     });
 
 function hideconsole() {
@@ -32,13 +32,13 @@ function showconsole() {
 }
 
 
-
+// https://www.w3schools.com/howto/howto_js_draggable.asp
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 //  if (document.getElementById(elmnt.id + "header")) {
     /* if present, the header is where you move the DIV from:*/
-  //  document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
- // } else {
+//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+//   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     document.getElementById("legend").onmousedown = dragMouseDown;
  
@@ -73,25 +73,7 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // jQuery load complete, do your magic                  //https://stackoverflow.com/questions/7496789/how-to-include-jquery-in-another-javascript-file
-
+//https://www.w3schools.com/howto/howto_js_draggable.asp
 
 
 
@@ -311,136 +293,8 @@ function dragElement(elmnt) {
       //      changes Luberth https://plnkr.co/edit/AufqNkQe4InMuU8v
       //      test for https://ldijkman.github.io/Ace_Seventh_Heaven/Hell.html 
       //    more? https://stackoverflow.com/questions/13815640/a-proper-wrapper-for-console-log-with-correct-line-number
+      // copyright dirk luberh dijkman
 
-
-
-      
-/*
-
-
-   document.addEventListener('DOMContentLoaded', function() {      // do next when document is loaded
-    //consoledragElement(document.getElementById("#console-log-div"));            // make editpopup draggable by mouse and touch
-  consoledragElement(document.getElementById("#legend"));
-}, false);
-
-/////////////////////////////////////////////////////////////////////
-// Make the DIV add or edit popup element draggable:
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
-// touch should be added == think it works , touch me
-// https://stackoverflow.com/questions/56703458/how-to-make-a-draggable-elements-for-touch-and-mousedrag-events
-// https://codepen.io/ldijkman/pen/abQxbdM
-//////////////////////////////////////////////////////////////////////
-function consoledragElement(elmnt) {
-  let pos1 = 0,
-      pos2 = 0,
-      pos3 = 0,
-      pos4 = 0;
-
-  let dragHandle = elmnt;    //dragElement(document.getElementById("#legend"));
-
-  if (dragHandle !== undefined) {
-    // if present, the header is where you move the DIV from:
-    dragHandle.onmousedown = dragMouseDown;
-    dragHandle.ontouchstart = dragMouseDown; //added touch event
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-    elmnt.ontouchstart = dragMouseDown; //added touch event
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-
-    //Get touch or click position
-    //https://stackoverflow.com/a/41993300/5078983
-    if (
-      e.type == "touchstart" ||
-      e.type == "touchmove" ||
-      e.type == "touchend" ||
-      e.type == "touchcancel"
-    ) {
-      let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
-      let touch = evt.touches[0] || evt.changedTouches[0];
-      x = touch.pageX;
-      y = touch.pageY;
-    } else if (
-      e.type == "mousedown" ||
-      e.type == "mouseup" ||
-      e.type == "mousemove" ||
-      e.type == "mouseover" ||
-      e.type == "mouseout" ||
-      e.type == "mouseenter" ||
-      e.type == "mouseleave"
-    ) {
-      x = e.clientX;
-      y = e.clientY;
-    }
-
-    console.log("drag start x: " + x + " y:" + y);
-
-    // get the mouse cursor position at startup:
-    pos3 = x;
-    pos4 = y;
-    document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
-  }
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-
-    //Get touch or click position
-    //https://stackoverflow.com/a/41993300/5078983
-    if (
-      e.type == "touchstart" ||
-      e.type == "touchmove" ||
-      e.type == "touchend" ||
-      e.type == "touchcancel"
-    ) {
-      let evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
-      let touch = evt.touches[0] || evt.changedTouches[0];
-      x = touch.pageX;
-      y = touch.pageY;
-    } else if (
-      e.type == "mousedown" ||
-      e.type == "mouseup" ||
-      e.type == "mousemove" ||
-      e.type == "mouseover" ||
-      e.type == "mouseout" ||
-      e.type == "mouseenter" ||
-      e.type == "mouseleave"
-    ) {
-      x = e.clientX;
-      y = e.clientY;
-    }
-
-    // calculate the new cursor position:
-    pos1 = pos3 - x;
-    pos2 = pos4 - y;
-    pos3 = x;
-    pos4 = y;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    console.log("drag end x: " + pos3 + " y:" + pos4);
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.ontouchcancel = null;                      // added touch event
-    document.ontouchend = null;                         // added touch event
-    document.onmousemove = null;
-    document.ontouchmove = null;                        // added touch event
-
-    selected_highlight=-1;                              // erase edit timeslot highlight
-  }
-} // end function dragMouseDown(e) {
-
-*/
 
 
   
